5188d0704fe05cbbaaeff3e608a81b4eeb5cbb6f
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2024-06-09 08:31:30 +0900

    Spotify API Client 生成

--- Backend/index.py
+++ Backend/index.py
@@ -14,6 +14,19 @@ def Read_Environment_File() -> bool:
 		verbose = True
 	)
 
+def Create_Spotify_Client() -> Spotify:
+	return  spotipy.Spotify(
+		auth_manager = SpotifyClientCredentials(
+			client_id = os.getenv(key = 'SPOTIFY_CLIENT_ID'),
+			client_secret = os.getenv(key = 'SPOTIFY_CLIENT_SECRET_KEY'),
+			requests_timeout = 30
+		),
+
+		language = 'ja',
+		requests_timeout = 30,
+		retries = 1
+	)
+
 if __name__ == '__main__':
 	logging.basicConfig(
 		level = logging.INFO,
@@ -22,6 +35,7 @@ if __name__ == '__main__':
 	)
 
 	if Read_Environment_File() == True:
+		client: Spotify = Create_Spotify_Client()
 		logging.info(msg = '処理が正常に終了しました。')
 	else:
 		logging.error(msg = '環境変数（.env）の読み込みに失敗しました。')
