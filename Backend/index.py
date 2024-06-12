ed598c3460d13f49bc633dbb0337394c641f00c4
masakazu-hirano <63486456+masakazu-hirano@users.noreply.github.com>
2024-06-12 16:53:13 +0900

    Spotify のアーティストページにあるプロフィール画像をダウンロードして R2 へ保存

--- Backend/index.py
+++ Backend/index.py
@@ -1,12 +1,15 @@
-import boto3
 import logging
 import os
+
+import boto3
 import requests
 import spotipy
 
-from botocore.config import Config
-from dotenv import load_dotenv
 from io import BytesIO
+
+from botocore.config import Config
+from botocore.exceptions import ClientError
+from dotenv import load_dotenv
 from PIL import Image
 from spotipy.client import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -41,6 +44,22 @@ def Create_Boto3_Client():
 		config = Config(signature_version = 'v4')
 	)
 
+def Upload_Artist_Profile_Image(client, file_name: str) -> None:
+	try:
+		client.head_object(
+			Bucket = 'av-manage',
+			Key = f"NiziU/{file_name}"
+		)
+	except ClientError:
+		client.upload_file(
+			Filename = f"./Backend/Downloads/{file_name}",
+			Bucket = 'av-manage',
+			Key = f"{artist_name}/{artist_name}_{artist_image_url.split(sep = '/')[-1]}.png",
+		)
+
+		logging.info(msg = f"ファイルアップロード完了: {file_name}")
+	os.remove(path = f"./Backend/Downloads/{file_name}")
+
 if __name__ == '__main__':
 	logging.basicConfig(
 		level = logging.INFO,
@@ -49,13 +68,27 @@ if __name__ == '__main__':
 	)
 
 	if Read_Environment_File() == True:
+		s3_client = Create_Boto3_Client()
 		spotify_client: Spotify = Create_Spotify_Client()
 
-		artist_information: dict = spotify_client.artist(artist_id = '')
+		artist_information: dict = spotify_client.artist(artist_id = '3z8diLlUCkN1j9N9ZdnfBJ')
 		artist_name: str = artist_information['name']
 		artist_image_url: str = artist_information['images'][0]['url']
 
-		s3_client = Create_Boto3_Client()
+		file_name: str = f"{artist_name}_{artist_image_url.split(sep = '/')[-1]}.png"
+		with Image.open(
+			fp = BytesIO(initial_bytes = requests.get(url = artist_image_url).content),
+			mode = 'r',
+			formats = ('BMP', 'GIF', 'JPEG', 'PNG', 'WEBP')
+		) as image_file:
+			image_file.save(
+				fp = f"./Backend/Downloads/{file_name}",
+				format = 'PNG',
+				compress_level = 0,
+				optimize = False
+			)
+
+		Upload_Artist_Profile_Image(s3_client, file_name)
 		logging.info(msg = '処理が正常に終了しました。')
 	else:
 		logging.error(msg = '環境変数（.env）の読み込みに失敗しました。')
